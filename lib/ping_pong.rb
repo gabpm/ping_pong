57038554064fa8c3c18c05a40ef198bfa0816dd7
zuriel ebron & Gabriel Puello <student@epicodus.com>
2015-02-09 11:18:53 -0800

    complete ping method in ruby

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,9 +3,12 @@ class Fixnum
     array =[]
     range = (1..self)
     range.each() do |n|
-      if n % 3 == 0
+      if n % 15 == 0
+      array.push("ping-pong")
+      elsif n % 3 == 0
         array.push("ping")
-      elsif
+      elsif n % 5 == 0
+        array.push("pong")
       else
         array.push(n)
       end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -4,18 +4,24 @@ require("ping_pong")
 
 describe('Fixnum#ping_pong') do
   it("will count up to given number within a range and push those numbers to an array") do
-  expect((2).ping_pong()).to(eq([1,2]))
+    expect((2).ping_pong()).to(eq([1,2]))
   end
 end
 
 describe("Fixnum#ping_pong") do
   it("returns ping if number is divisible by three") do
-  expect((3).ping_pong()).to(eq([1,2,"ping"]))
+    expect((3).ping_pong()).to(eq([1,2,"ping"]))
   end
 end
 
 describe("Fixnum#ping_pong") do
   it("returns pong if number is divisible by five") do
-  expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+    expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
+end
+
+describe("Fixnum#ping_pong") do
+  it ("returns ping-pong if number is divisible by 15") do
+    expect((15).ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
   end
 end
